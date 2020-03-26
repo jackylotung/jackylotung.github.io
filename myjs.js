ce0c04722cf156916336f2fc526b91c894b0e48a
羅子渝 <godcurry@luoziyude-MacBook-Pro.local>
2020-03-26 12:59:58 +0800

    js

--- myjs.js
+++ myjs.js
@@ -1,6 +1,6 @@
 $(function() {
     $("#Page_2").mouseover(function() {
-        $("#text1_inP2").animate({ left: '-20vw' }, 1500);
-        $("#text2_inP2").animate({ left: '20vw' }, 1500);
+        $("#text1_inP2").animate({ left: '0vw' }, 1500);
+        $("#text2_inP2").animate({ left: '15vw' }, 1500);
     });
 })
