2fbad292db8d559a6b2d5f667311a65706707330
羅子渝 <godcurry@luoziyude-MacBook-Pro.local>
2020-03-26 13:11:48 +0800

    js

--- myjs.js
+++ myjs.js
@@ -3,13 +3,13 @@ $("#Page_2").mouseover(function() {
     $("#text2_inP2").animate({ left: '15vw' }, 1500);
 });
 $("#page2").mouseleave(function() {
-    $("#text1_inPg2").animate({ left: '-100vw' }, 1200);
-    $("#text2_inPg2").animate({ right: '-100vw' }, 1200);
+    $("#text1_inP2").animate({ left: '-100vw' }, 1200);
+    $("#text2_inP2").animate({ right: '-100vw' }, 1200);
 })
 var tmp_opacity = 0;
 var deepen1 = setInterval(function() { Deepen() }, 120);
 
 function Deepen() {
     tmp_opacity += 0.07;
-    $(".page1").css("opacity", tmp_opacity);
+    $("#page1").css("opacity", tmp_opacity);
 }
